db406b208b8b348c8e695bb05805804c8ecc16e2
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2019-10-20 19:54:56 -0400

    adding destructuring onto objects

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const forecast = require('./forecast.js');
 const geocode = require('./geocode.js');
 
 const userInput = process.argv[2];
-geocode(userInput, (error, geodata) => {
 
-    forecast(geodata.latitude, geodata.longitude, (error, data) => {
+geocode(userInput, (error, {latitude, longitude, place_name}) => {
+    if(!userInput){
+       return console.log(error, "Please enter the location you want to know weather from!!");
+    }
+
+    forecast(latitude, longitude, (error, data) => {
         console.log("================================");
-        console.log(geodata.place_name);
+        console.log(place_name);
         console.log(data);
     });
 
 });
+
+
 
