5af3681129efc701bcbf95f51bccc6ef40870d01
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2019-10-20 19:54:23 -0400

    adding destructuring onto objects

--- geocode.js
+++ geocode.js
@@ -4,16 +4,16 @@ const geocode = (userInput, callback) => {
 
 
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + userInput + ".json?limit=2&access_token=pk.eyJ1IjoiaGFyMTIzIiwiYSI6ImNrMW83ZHJsbDBpN3ozcXFmcjd4Zm11dHQifQ.QcjxXW4bwnwGHUiKMtSbpw"
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, {body}) => {
 
         if (error) {
             callback("Problem with either URL or internet connection!!");
         }
         else
             callback(undefined, {
-                longitude: response.body.features[0].center[0],
-                latitude: response.body.features[0].center[1],
-                place_name: response.body.features[0].place_name
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                place_name: body.features[0].place_name
             })
     });
 
